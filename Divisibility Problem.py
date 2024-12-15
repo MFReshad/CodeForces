fe4dcea451903b60e066c6e55acb6da8eab6e854
Muhtasim Ferdous Reshad <60946283+MFReshad@users.noreply.github.com>
2024-12-16 02:49:16 +0600

    26-07-24

--- Divisibility Problem.py
+++ Divisibility Problem.py
@@ -1,16 +1,22 @@
 t = int(input().strip())
 i=0
-def is_divisible(a,b,n):
-    if a%b==0:
-        return n
-    else:
-        a+=1
-        n+=1
-        return is_divisible(a,b,n)
+# def is_divisible(a,b,n):
+#     if a%b==0:
+#         return n
+#     else:
+#         a+=1
+#         n+=1
+#         return is_divisible(a,b,n)
 
 while i<t:
     a, b = map(int, input().strip().split())
-    global count
-    count = 0
-    print(is_divisible(a,b,count))
+    # global count
+    # count = 0
+    # print(is_divisible(a,b,count))
+    '''chtgpt'''
+    remeider = a%b
+    if(remeider==0):
+        print(remeider)
+    else:
+        print(b-remeider)
     i+=1
